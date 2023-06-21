e2cd0cdaa54a25d97993cc3af3cc418edf4ab44a
MedoAngel <actionman14.ma@gmail.com>
2023-06-21 14:56:09 +0100

    add ThemeModeSwitch to components

---
+++ src/components/ThemeModeSwitch.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col>
+        <v-icon>fas fa-sun</v-icon>
+      </v-col>
+      <v-col cols="auto" align="top">
+        <v-switch hide-details class="pa-auto" @update:modelValue="changeMode" inset />
+      </v-col>
+      <v-col>
+        <v-icon>fas fa-moon</v-icon>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["changeMode"]);
+
+function changeMode(isDarkMode: boolean): void {
+  emit("changeMode", isDarkMode);
+}
+</script>
