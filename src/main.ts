b2ad4dd9cbd3d56093e94a4a23a1a71a6ae401eb
MedoAngel <actionman14.ma@gmail.com>
2023-06-21 14:58:06 +0100

    add font-awesome as the default icon set to vuetify

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,19 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@fortawesome/fontawesome-free/css/all.css";
+import { aliases, fa } from "vuetify/iconsets/fa";
 
-const vuetify = createVuetify({ components, directives });
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+});
 
 createApp(App).use(store).use(router).use(vuetify).mount("#app");
