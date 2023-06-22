0e71ac15cd00ac8fa1973cacf7814b10862573a3
MedoAngel <actionman14.ma@gmail.com>
2023-06-22 12:20:56 +0100

    refactor CardForm to use BaseForm

--- src/components/card/CardForm.vue
+++ src/components/card/CardForm.vue
@@ -1,32 +1,23 @@
 <template>
   <div>
-    <v-card class="pa-3">
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-text-field v-model="card.title" label="Title" variant="outlined" />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-textarea v-model="card.content" label="Content" variant="outlined" />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col align="center">
-            <v-btn @click="emitSave" color="primary"> Save </v-btn>
-          </v-col>
-          <v-col align="center">
-            <v-btn @click="emit('cancel')"> Cancel </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+    <BaseForm @cancel="emit('cancel')">
+      <v-row>
+        <v-col>
+          <v-text-field v-model="card.title" label="Title" variant="outlined" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-textarea v-model="card.content" label="Content" variant="outlined" />
+        </v-col>
+      </v-row>
+    </BaseForm>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import BaseForm from "@/components/BaseForm.vue";
 
 const props = defineProps(["board", "column", "card"]);
 const emit = defineEmits(["cancel", "save"]);
