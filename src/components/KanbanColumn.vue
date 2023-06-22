9b983a3616d2e6bfeedc724b439159faed1e5af0
MedoAngel <actionman14.ma@gmail.com>
2023-06-22 11:09:12 +0100

    add CardForm to card components

--- src/components/KanbanColumn.vue
+++ src/components/KanbanColumn.vue
@@ -13,7 +13,8 @@
                 density="compact"
                 size="small"
                 variant="icon"
-                icon="fas  fa-plus"
+                icon="fas fa-plus"
+                @click="showCardForm = true"
               ></v-btn>
             </v-col>
           </v-row>
@@ -21,11 +22,17 @@
       </template>
       <KanbanCard class="py-2 px-0" v-for="(card, idx) in props.cards" v-bind="card" :key="idx" />
     </v-card>
+    <v-dialog class="v-col-3" v-model="showCardForm">
+      <CardForm @save="handleSave" @cancel="showCardForm = false" v-if="showCardForm" />
+    </v-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import KanbanCard from "@/components/card/KanbanCard.vue";
+import CardForm from "@/components/card/CardForm.vue";
 
 const props = defineProps(["name", "cards"]);
+const showCardForm = ref(false);
 </script>
---
+++ src/components/card/CardForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-card class="pa-3">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field v-model="card.title" label="Title" variant="outlined" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-textarea v-model="card.content" label="Content" variant="outlined" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col align="center">
+            <v-btn @click="emitSave" color="primary"> Save </v-btn>
+          </v-col>
+          <v-col align="center">
+            <v-btn @click="emit('cancel')"> Cancel </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps(["board", "column", "card"]);
+const emit = defineEmits(["cancel", "save"]);
+
+const card = ref({
+  board: props?.board,
+  column: props?.column,
+  title: props?.card?.title,
+  content: props?.card?.content,
+});
+</script>
