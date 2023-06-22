29d88771e286a0f050500648c9264f48d7a89d17
MedoAngel <actionman14.ma@gmail.com>
2023-06-23 00:07:03 +0100

    remove board prop from KanbanColumn

--- src/components/board/BoardWindow.vue
+++ src/components/board/BoardWindow.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="pa-3">
     <v-row style="height: calc(100vh - 56px)" class="flex-nowrap overflow-auto">
-      <v-col cols="3" v-for="(column, idx) in props.columns" :key="idx">
-        <KanbanColumn :board="props.name" v-bind="column" />
+      <v-col cols="3" v-for="(column, idx) in columns" :key="idx">
+        <KanbanColumn v-bind="column" />
       </v-col>
     </v-row>
   </div>
