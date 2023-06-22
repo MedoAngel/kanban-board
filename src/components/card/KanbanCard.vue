69b6f6d2fb0c20915f1f4f325b1bed8f1a8e1994
MedoAngel <actionman14.ma@gmail.com>
2023-06-22 14:04:55 +0100

    remove edit and delete buttons

--- src/components/card/KanbanCard.vue
+++ src/components/card/KanbanCard.vue
@@ -7,18 +7,6 @@
             <v-col cols="10">
               <v-card-title>{{ props.title }}</v-card-title>
             </v-col>
-            <v-col class="pa-1" cols="1">
-              <v-btn
-                :ripple="false"
-                variant="flat"
-                density="compact"
-                size="x-small"
-                icon="fas fa-trash-alt"
-              ></v-btn>
-            </v-col>
-            <v-col class="pa-1" cols="1">
-              <v-btn :ripple="false" density="compact" size="x-small" icon="fas fa-pen"></v-btn>
-            </v-col>
           </v-row>
         </v-container>
       </template>
