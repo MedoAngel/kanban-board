05263a88f853a6de418209982d1eeafd9d479fe6
MedoAngel <actionman14.ma@gmail.com>
2023-06-22 12:42:41 +0100

    refactor BoardForm to use BaseForm

--- src/components/board/BoardForm.vue
+++ src/components/board/BoardForm.vue
@@ -1,27 +1,18 @@
 <template>
   <div>
-    <v-card class="pa-3">
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-text-field v-model="boardName" label="Board Name" variant="outlined" />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col align="center">
-            <v-btn @click="emitSave" color="primary"> Save </v-btn>
-          </v-col>
-          <v-col align="center">
-            <v-btn @click="emit('cancel')"> Cancel </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+    <BaseForm @save="emitSave" @cancel="emit('cancel')">
+      <v-row>
+        <v-col>
+          <v-text-field v-model="boardName" label="Board Name" variant="outlined" />
+        </v-col>
+      </v-row>
+    </BaseForm>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, toValue } from "vue";
+import BaseForm from "@/components/BaseForm.vue";
 
 const emit = defineEmits(["cancel", "save"]);
 const boardName = ref("");
