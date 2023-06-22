eb02c35174ab42bb209c6bb0120975902a4c3732
MedoAngel <actionman14.ma@gmail.com>
2023-06-22 12:19:10 +0100

    add BaseForm to components

---
+++ src/components/BaseForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-card class="pa-3">
+      <v-container>
+        <v-form @submit.prevent="emit('save')">
+          <slot></slot>
+          <v-row>
+            <v-col align="center">
+              <v-btn type="submit" color="primary"> Save </v-btn>
+            </v-col>
+            <v-col align="center">
+              <v-btn @click="emit('cancel')"> Cancel </v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["cancel", "save"]);
+</script>
