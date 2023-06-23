fa9f14cdd91a42497059269633769c5f9235df61
MedoAngel <actionman14.ma@gmail.com>
2023-06-23 22:38:05 +0100

    add getColumnCards function to cards api

---
+++ src/api/cards.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosPromise } from "axios";
+import { BASE_URL } from "../../server.config";
+
+const cardsUrl = `${BASE_URL}/cards`;
+
+export async function getColumnCards(column: Column): AxiosPromise {
+  return await axios.get(`${cardsUrl}?columnId=${column.id}&boardId=${column.boardId}`);
+}
