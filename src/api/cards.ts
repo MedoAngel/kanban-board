1265b253e5a14f519d42233e1c38d526693bef1a
MedoAngel <actionman14.ma@gmail.com>
2023-06-23 23:38:22 +0100

    add sendSaveCard to cards api

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -6,3 +6,7 @@ const cardsUrl = `${BASE_URL}/cards`;
 export async function getColumnCards(column: Column): AxiosPromise {
   return await axios.get(`${cardsUrl}?columnId=${column.id}&boardId=${column.boardId}`);
 }
+
+export async function sendSaveCard(card: Card): AxiosPromise {
+  return await axios.post(cardsUrl, card);
+}
