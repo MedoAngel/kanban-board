43dd671caf7c851abce84e28d43eae9f0a02197e
MedoAngel <actionman14.ma@gmail.com>
2023-06-25 00:15:07 +0100

    use router for views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,24 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import Home from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: Home,
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/share/card/:id",
+    name: "share",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/ShareView.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-layout>
+    <v-navigation-drawer class="bg-deep-purple px-3" permanent>
+      <template #prepend>
+        <AddNewBtn @click="showBoardForm = true">Add New Board</AddNewBtn>
+      </template>
+
+      <v-list color="transparent">
+        <v-tabs v-model="currentBoard" direction="vertical">
+          <v-tab v-for="(board, idx) in boards" :key="idx" :value="board">
+            {{ board.name }}
+          </v-tab>
+        </v-tabs>
+      </v-list>
+
+      <template #append>
+        <ThemeModeSwitch @changeMode="changeThemeMode" />
+      </template>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-toolbar :title="currentBoard.name" density="comfortable">
+        <template #append>
+          <AddNewBtn @click="showColumnForm = true">Add New Column</AddNewBtn>
+        </template>
+      </v-toolbar>
+
+      <v-window v-model="currentBoard">
+        <BoardWindow v-bind="board" v-for="(board, idx) in boards" :value="board" :key="idx" />
+      </v-window>
+    </v-main>
+  </v-layout>
+
+  <v-dialog class="v-col-3" v-model="showBoardForm">
+    <BoardForm @save="saveBoard" @cancel="showBoardForm = false" v-if="showBoardForm" />
+  </v-dialog>
+
+  <v-dialog class="v-col-3" v-model="showColumnForm">
+    <ColumnForm @save="saveColumn" @cancel="showColumnForm = false" v-if="showColumnForm" />
+  </v-dialog>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
+import { useTheme } from "vuetify";
+import ThemeModeSwitch from "@/components/ThemeModeSwitch.vue";
+import BoardForm from "@/components/board/BoardForm.vue";
+import BoardWindow from "@/components/board/BoardWindow.vue";
+import AddNewBtn from "@/components/AddNewBtn.vue";
+import ColumnForm from "@/components/column/ColumnForm.vue";
+import { sendSaveBoard, getBoards } from "@/api/boards";
+import { sendSaveColumn } from "@/api/columns";
 
-export default defineComponent({
-  name: "HomeView",
-  components: {},
+const theme = useTheme();
+
+const showBoardForm = ref(false);
+const currentBoard = ref("");
+const showColumnForm = ref(false);
+const boards = ref([]);
+
+getBoards().then(({ data }) => {
+  boards.value = data;
+  currentBoard.value = data[0];
 });
+
+function changeThemeMode(toDarkMode: boolean): void {
+  theme.global.name.value = toDarkMode ? "dark" : "light";
+}
+
+function saveBoard(boardName: string): void {
+  const board = { name: boardName, columns: [] };
+
+  sendSaveBoard(board).then(({ data }) => {
+    boards.value.push(data);
+
+    showBoardForm.value = false;
+  });
+}
+
+function saveColumn(columnName: string): void {
+  const boardId = currentBoard.value.id;
+  const column = { name: columnName, boardId };
+
+  sendSaveColumn(column).then(({ data }) => {
+    const board = boards.value.find(({ id }) => id === boardId);
+
+    if (board) {
+      board.newColumn = data;
+    }
+
+    showColumnForm.value = false;
+  });
+}
 </script>
