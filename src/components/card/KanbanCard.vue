d88540e1d7200158bde981a63deda4928264c40e
MedoAngel <actionman14.ma@gmail.com>
2023-06-24 22:51:51 +0100

    add share functionality to KanbanCard

--- src/components/card/KanbanCard.vue
+++ src/components/card/KanbanCard.vue
@@ -2,21 +2,47 @@
   <div>
     <v-card class="text-left">
       <template #title>
-        <v-container class="pa-1">
-          <v-row>
-            <v-col cols="10">
-              <v-card-title>{{ props.title }}</v-card-title>
-            </v-col>
-          </v-row>
-        </v-container>
+        <v-card-title>{{ card.title }}</v-card-title>
       </template>
       <template #text>
-        <v-card-text class="pa-0">{{ props.content }}</v-card-text>
+        <v-card-text class="pa-0">{{ card.content }}</v-card-text>
       </template>
+      <v-row justify="end">
+        <v-col cols="auto" align="end">
+          <v-tooltip location="bottom">
+            <template #activator="{ props }">
+              <v-btn
+                v-bind="props"
+                @click="shareLink"
+                :ripple="false"
+                variant="plain"
+                icon="fas fa-share-alt"
+              >
+              </v-btn>
+            </template>
+            Share Card
+          </v-tooltip>
+        </v-col>
+      </v-row>
     </v-card>
+
+    <v-dialog class="v-col-4" v-model="showShareDialog">
+      <LinkPrompt :url="cardLink" />
+    </v-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps(["title", "content"]);
+import { ref } from "vue";
+import LinkPrompt from "@/components/LinkPrompt.vue";
+import { BASE_URL } from "../../../server.config";
+
+const card = defineProps(["id", "title", "content"]);
+
+const showShareDialog = ref(false);
+const cardLink = ref(`${BASE_URL}/share/card/${card.id}`);
+
+function shareLink() {
+  showShareDialog.value = true;
+}
 </script>
