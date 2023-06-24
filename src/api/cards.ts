a0f4f0f43ec1c9feb47e4c931c8fee7be40818c1
MedoAngel <actionman14.ma@gmail.com>
2023-06-24 16:21:15 +0100

    refactor getColumnCards to sort and order results by position

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -4,7 +4,16 @@ import { BASE_URL } from "../../server.config";
 const cardsUrl = `${BASE_URL}/cards`;
 
 export async function getColumnCards(column: Column): AxiosPromise {
-  return await axios.get(`${cardsUrl}?columnId=${column.id}&boardId=${column.boardId}`);
+  const paramsList = [
+    `columnId=${column.id}`,
+    `boardId=${column.boardId}`,
+    `_sort=position`,
+    `_order=asc`,
+  ];
+  const params = paramsList.join("&");
+  const requestLink = `${cardsUrl}?${params}`;
+
+  return await axios.get(requestLink);
 }
 
 export async function sendSaveCard(card: Card): AxiosPromise {
