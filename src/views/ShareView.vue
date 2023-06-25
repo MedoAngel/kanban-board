7ba9985be792d9937a4325f81b9b7bc1224b778c
MedoAngel <actionman14.ma@gmail.com>
2023-06-25 01:37:13 +0100

    add ShareView to views

---
+++ src/views/ShareView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-dialog persistent class="v-col-3" :model-value="true">
+      <KanbanCard v-bind="card" />
+    </v-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import KanbanCard from "@/components/card/KanbanCard.vue";
+import { getCardById } from "@/api/cards";
+
+const route = useRoute();
+const cardId = route.params.id;
+
+const card = ref({});
+
+getCardById(cardId).then(({ data }) => {
+  card.value = data;
+});
+</script>
+
+<style scoped></style>
